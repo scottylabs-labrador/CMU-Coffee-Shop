7fe2c475281c960caf87e588370b4fbd9f01cdd7
Ankita Varigonda <ankitavarigonda@Ankitas-MacBook-Pro.local>
2025-11-01 11:29:00 -0400

    home page set-up

--- coffeeshop/src/App.tsx
+++ coffeeshop/src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
 
+      <div className="container-fluid text-center pt-5 display-1">
+        Coffee Shop
+      </div>
+
+      <img src="placeholder.png" alt="Coffee Cup"></img>
+
     </div>
   );
 }
