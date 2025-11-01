e70a383f8bed5935367c180d778bd87224bf9074
Ankita Varigonda <ankitavarigonda@Ankitas-MacBook-Pro.local>
2025-11-01 12:25:31 -0400

    home page basic code

--- coffeeshop/src/App.tsx
+++ coffeeshop/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import coffeecup from './Assets/placeholder.png';
 
 function App() {
   return (
@@ -9,9 +10,18 @@ function App() {
         Coffee Shop
       </div>
 
-      <img src="placeholder.png" alt="Coffee Cup"></img>
+      <img src={coffeecup} className="mt-5 round" alt="coffeecup" />
+
+      <div className="row mt-5">
+      <div className="col-3"></div>
+      <div className="col-6">
+        <div className="alert alert-secondary h3">Find a CMU cafe to study in!</div>
+      </div>
+      <div className="col-3"></div>
+    </div>
 
     </div>
+
   );
 }
 
