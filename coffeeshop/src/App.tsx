48bf5fd28974fb7bbff5528bea48d8d8e7cb0d9a
Ankita Varigonda <avarigonda18@gmail.com>
2025-11-15 20:35:26 -0500

    maps functional! w/ two pins added

--- coffeeshop/src/App.tsx
+++ coffeeshop/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Home';
 import Guide from './Guide';
 import Maps from './Maps';
+import sampleData from './SampleData.json';
 
 function App() {
   const [page, setPage] = useState(0);
@@ -11,7 +12,7 @@ function App() {
 
       {page === 0 && <Home toGuide={() => setPage(1)} />}
       {page === 1 && <Guide toMaps={() => setPage(2)}/>}
-      {page === 2 && <Maps />}
+      {page === 2 && <Maps locations={sampleData} />}
   
     </div>
   );
--- coffeeshop/src/Maps.tsx
+++ coffeeshop/src/Maps.tsx
@@ -1,15 +1,62 @@
 import coffeecup from './Assets/placeholder.png';
-const apiKey = process.env.MAP_KIT_KEY;
+import { useMemo, useState, useEffect } from 'react';
+import { Map, Marker, ColorScheme, PointOfInterestCategory } from 'mapkit-react';
+import './App.css';
+const tkn = "token here";
 
-function Maps() {
-    return (
-        <>
-        
-        <div><img src={coffeecup} className="m-3 tlround" alt="coffeecup" /></div>
-
-        </>
-    )
+interface Location {
+    Name: string;
+    Coordinates: {
+        lat: number;
+        lng: number;
+    };
+    Occupancy: number;
+    "Noise Level": number;
 }
 
+function Maps({ locations }: { locations: Location[] | undefined }) {
+    const [selectedLocationIndex, setSelectedLocationIndex] = useState<number>();
+
+    const initialRegion = useMemo(
+    () => ({
+      centerLatitude: 40.443,      // example latitude
+      centerLongitude: -79.943,    // example longitude
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    }),
+    []
+  );
+
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Map
+        token={tkn}
+        colorScheme={ColorScheme.Dark}
+        initialRegion={initialRegion}
+        showsUserLocationControl
+        allowWheelToZoom
+      >
+        {(locations ?? []).map((location, index) => (
+          <Marker
+            key={location.Name}
+            latitude={location.Coordinates.lat}
+            longitude={location.Coordinates.lng}
+            glyphText={location.Name.charAt(0)} // single letter for now
+            onSelect={() => setSelectedLocationIndex(index)} //mechanism for action on select
+            onDeselect={() => {
+              if (selectedLocationIndex === index) setSelectedLocationIndex(undefined);
+            }}
+          />
+        ))}
+      </Map>
+    </div>
+  );
+
+}
+
+
+/*
+<div><img src={coffeecup} className="m-3 tlround" alt="coffeecup" /></div>
+*/
 
 export default Maps;
